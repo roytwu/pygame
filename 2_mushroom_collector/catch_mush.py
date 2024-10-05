740e04ce6b7b013cbf92d44bbe900a6163a049c5
Roy Wu <roywu2018@gmail.com>
2024-10-04 22:12:17 -0400

    adding icons and comments

--- 2_mushroom_collector/catch_mush.py
+++ 2_mushroom_collector/catch_mush.py
@@ -3,20 +3,29 @@ Author:      Roy Wu, Summer Wu
 Description: The princess version of the pac-man game
 History:     09/19/2024, initial version
 """
-
 import pgzrun #* pygame zero
 from random import randint
+from time   import time
 
-WIDTH = 900
+WIDTH  = 900
 HEIGHT = 600
-score = 0
+score  = 0
 game_over = False
 
-peach = Actor("princess_100.png")
-mush = Actor("mushroom.png")
-
+# peach = Actor("princess_100.png")
+peach = Actor("monster_128.png")
+mush  = Actor("mushroom.png")
 peach.pos = 100, 100
-mush.pos = 200, 200 
+mush.pos  = 200, 200 
+
+def place_mush():
+    mush.x = randint(20, (WIDTH-20))
+    mush.y = randint(20, (HEIGHT-20))
+
+def time_up():
+    global game_over
+    game_over = True
+
 
 def draw():
     screen.fill("black")
@@ -27,16 +36,10 @@ def draw():
     if game_over:
         screen.fill("pink")
         screen.draw.text("Final Score: "+ str(score), 
-                         color = "green", topleft = (10, 10), fontsize = 60)
+                         color = "blue", topleft = (10, 10), fontsize = 60)
 
-def place_mush():
-    mush.x = randint(20, (WIDTH-20))
-    mush.y = randint(20, (HEIGHT-20))
 
-def time_up():
-    global game_over
-    game_over = True
-
+#* called repeatedly, 60 times a second
 def update():
     global score
     step_size = 6
@@ -57,14 +60,15 @@ def update():
         peach.y = peach.y + step_size
         if peach.y >= HEIGHT:
             peach.y = 0
-    
-    mush_get = peach.colliderect(mush)
 
+    mush_get = peach.colliderect(mush)
     if mush_get == True:
         score = score + 10
         place_mush()
 
 
+#* call time_up() after 20 seconds
 clock.schedule(time_up, 20.0)
+
 place_mush()
 pgzrun.go()
